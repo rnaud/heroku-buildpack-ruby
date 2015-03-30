ef4b0519c182d1e0cf546775be81044481cf2a10
Michael Righi <michael@righi.me>
2015-03-30 00:04:54 -0700

    Force bundler version to override possible newer versions

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -482,7 +482,7 @@ WARNING
       log("bundle") do
         bundle_without = env("BUNDLE_WITHOUT") || "development:test"
         bundle_bin     = "bundle"
-        bundle_command = "#{bundle_bin} install --without #{bundle_without} --path vendor/bundle --binstubs #{bundler_binstubs_path}"
+        bundle_command = "#{bundle_bin} install _#{BUNDLER_VERSION}_ --without #{bundle_without} --path vendor/bundle --binstubs #{bundler_binstubs_path}"
         bundle_command << " -j4"
 
         if bundler.windows_gemfile_lock?
